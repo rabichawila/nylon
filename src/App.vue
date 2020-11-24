047c6403360d8d5286fa805c8af1be4353671383
rabichawila <rabichawila@gmail.com>
2020-11-24 02:41:19 +0200

    created header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
-<div class="text-center">
-  <img alt="Vue logo" class="mx-auto mt-40" src="./assets/logo.png">
+<div class="container mx-auto my-5">
+  <div class="grid grid-cols-12 xxs:px-3 px-0">
+    <div class="border-b border-black xxs:col-span-12 md:col-span-7 lg:col-span-6 2xl:col-span-5" id="heading-cont">
+      <p class="uppercase xxs:tracking-tight md:tracking-tightest xxs:pb-10 sm:pb-2 bg-right-bottom xxs:text-6xl md:text-8xl lg:text-9xl bg-signature bg-no-repeat xxs:font-semibold sm:font-normal md:font-semibold" id="heading">Latest</p>
+    </div>
+  </div>
+
+    <div class="grid grid-cols-12">
+
+      <div class="xxs:col-span-12 md:col-span-5 mt-5">
+        <p class="text-sm">NYLON NINETEEN</p>
+      </div>
+    </div>
 </div>
 </template>
 
@@ -15,5 +26,72 @@ export default {
 </script>
 
 <style>
+ 
+ #heading{
+   /* background: url(https://cdn2.bustle.com/nylon/2020/scribble-920a660055.svg) no-repeat; */
+   background-position: right;
+    padding-top: 15px;
+   padding-bottom: 70px;
+   margin-bottom: -50px; 
+   margin-right: -10px;
+   font-size: 80px;
+    background-size: 150px 100px;
+ }
+ 
+@media (min-width: 576px) { 
+  
+ }
+
+
+@media (min-width: 768px) { 
+   #heading{
+    padding-top: 60px;
+    padding-bottom: 200px;
+    margin-bottom: -200px; 
+    margin-right: -465px;
+    background-size: 350px 250px;
+   font-size: 250px;
+  }  
+ }
+
+
+@media (min-width: 992px) { 
+   #heading{
+    padding-top: 50px;
+    padding-bottom: 200px;
+    margin-bottom: -200px; 
+    margin-right: -320px;
+    background-size: 240px 200px;
+  }
+ }
+
+@media (min-width: 1440px) { 
+   #heading{
+    padding-top: 50px;
+    padding-bottom: 200px;
+    margin-bottom: -200px; 
+    margin-right: -195px;
+    background-size: 240px 200px;
+  }
+ }
+
+
+
+
+@media (min-width: 2550px) { 
+  #heading{
+    padding-top: 50px;
+    padding-bottom: 200px;
+    margin-bottom: -200px; 
+    margin-right: -195px;
+    background-size: 240px 200px;
+  }
+ }
+
+
+#heading::first-letter{
+  margin-right: -10px; 
+}
+
 
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
+
   future: {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
@@ -9,9 +10,55 @@ module.exports = {
     './src/**/*.vue',
   ],
   theme: {
-    fontFamily: {
-      sans: ['Inter', ...defaultTheme.fontFamily.sans],
+    extend: {
+      backgroundImage: theme => ({
+       'signature': "url('https://cdn2.bustle.com/nylon/2020/scribble-920a660055.svg')",
+      })
+    },    
+    screens: {
+      'xxs': '320px',
+      // => @media (min-width: 320px) { ... }
+
+      'xs': '375px',
+      // => @media (min-width: 375px) { ... }
+
+      'xs': '425px',
+      // => @media (min-width: 425px) { ... }
+
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
     },
+
+    letterSpacing: {
+      tightest: '-.075em',
+       tighter: '-.05em',
+      tight: '-.025em',
+       normal: '0',
+      wide: '.025em',
+       wider: '.05em',
+      widest: '.1em',
+      widest: '.25em',
+     },
+
+    backgroundPosition: {
+      'top-5': 'right top 1rem',
+      'bottom-5': 'right bottom -1rem',
+    },
+    padding:{
+      'min-40': '-50px'
+    }     
   },
   variants: {},
   plugins: [],
