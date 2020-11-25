ebb15d986aace10641751dfaebb26449fce122f1
rabichawila <rabichawila@gmail.com>
2020-11-26 00:29:43 +0200

    Fix issues

--- src/App.vue
+++ src/App.vue
@@ -24,49 +24,57 @@
 
 
           <div class="xxs:col-span-12 md:col-span-6 mb-3 z-40 xxs:mb-6 border-b md:border-none border-black">
-            <div class="h-full grid grid-cols-12 place-content-end">
+            <div class="h-full grid grid-cols-12 content-end ">
               <div class="col-span-12 order-2 md:order-1 mb-5 md:mb-0">
                 <img src="/imgs/stal.jpg" class="object-cover h-auto w-full" alt="" srcset="">
               </div>
-              <div class="col-span-12 order-1 md:order-2">                
+              <div class="col-span-12 order-1 md:order-2 tSD">
                 <p class="text-sm lg:text-xs tracking-tight text-gray-700 my-2 mb-2 uppercase small-heading">POP-PUNK WEEK</p>
-                <p class="text-xl md:text-base text-right leading-4 font-medium uppercase tracking-tighter text-gray-900 my-4 mb-2" style="height: 45px;">Punk gets its overdue fashion comeback for 2020 and beyond</p>
+                <div class="flex flex-row-reverse">
+                  <p class="atX w-4/5 text-2xl md:text-base text-right font-medium uppercase tracking-tighter text-gray-900 my-4 mb-2">Punk gets its overdue fashion comeback for 2020 and beyond</p>
+                </div>
               </div>
             </div>
           </div>
 
           <div class="xxs:col-span-12 md:col-span-6 mb-3 z-40 xxs:mb-6 border-b md:border-none border-black">
-            <div class="h-full grid grid-cols-12 place-content-end">
+            <div class="h-full grid grid-cols-12 content-end ">
               <div class="col-span-12 xxs:order-2 md:order-1 mb-5 md:mb-0">
                 <img src="/imgs/pop.jpg" class="object-cover h-auto w-full" alt="" srcset="">
               </div>
-              <div class="col-span-12 order-1 md:order-2">                
+              <div class="col-span-12 order-1 md:order-2 tSD">
                 <p class="text-sm lg:text-xs tracking-tight text-gray-700 my-2 mb-2 uppercase small-heading">POP-PUNK WEEK</p>
-                <p class="text-xl md:text-base text-right leading-4 font-medium uppercase tracking-tighter text-gray-900 my-4 mb-2" style="height: 45px;">private icon: hayley williams</p>
+                <div class="flex flex-row-reverse">
+                  <p class="atX w-4/5 text-2xl md:text-base text-right font-medium uppercase tracking-tighter text-gray-900 my-4 mb-2">private icon: hayley williams</p>
+                </div>
               </div>
             </div>
           </div>
 
           <div class="xxs:col-span-12 md:col-span-6 mb-3 xxs:mb-6 border-b md:border-none border-black">
-            <div class="h-full grid grid-cols-12 place-content-end">
-              <div class="col-span-12">
+            <div class="h-full grid grid-cols-12 content-end ">
+              <div class="col-span-12 xxs:order-2 md:order-1 mb-5 md:mb-0">
                 <img src="/imgs/img2.jpg" class="object-cover h-auto w-full" alt="" srcset="">
               </div>
-              <div class="col-span-12">                
+              <div class="col-span-12 order-1 md:order-2 tSD">
                 <p class="text-sm lg:text-xs tracking-tight text-gray-700 my-2 mb-2 uppercase small-heading">POP-PUNK WEEK</p>
-                <p class="text-xl md:text-base text-right leading-4 font-medium uppercase tracking-tighter text-gray-900 my-4 mb-2" style="height: 45px;">Oral history of the mid-2000s scene queens</p>
+                <div class="flex flex-row-reverse">
+                  <p class="atX w-4/5 text-2xl md:text-base text-right font-medium uppercase tracking-tighter text-gray-900 my-4 mb-2">Oral history of the mid-2000s scene queens</p>
+                </div>
               </div>
             </div>
           </div>
 
           <div class="xxs:col-span-12 md:col-span-6 mb-3 xxs:mb-6 border-b md:border-none border-black">
-            <div class="h-full grid grid-cols-12 place-content-end">
-              <div class="col-span-12">
+            <div class="h-full grid grid-cols-12 content-end ">
+              <div class="col-span-12 xxs:order-2 md:order-1 mb-5 md:mb-0">
                 <img src="/imgs/img3.jpg" class="object-cover h-auto w-full" alt="" srcset="">
               </div>
-              <div class="col-span-12">                
+              <div class="col-span-12 order-1 md:order-2 tSD">
                 <p class="text-xs tracking-tight text-gray-700 my-2 mb-2 uppercase small-heading">POP-PUNK WEEK</p>
-                <p class="text-xl md:text-base text-right leading-4 font-medium uppercase tracking-tighter text-gray-900 my-4 mb-2" style="height: 45px;">megan thee stallion's new album will feature sza, popcaan & city girls</p>
+                <div class="flex flex-row-reverse">
+                  <p class="atX w-4/5 text-2xl md:text-base text-right font-medium uppercase tracking-tighter text-gray-900 my-4 mb-2">megan thee stallion's new album will feature sza, popcaan & city girls</p>
+                </div>
               </div>
             </div>
           </div>
@@ -75,6 +83,7 @@
 
         </div>
       </div>
+      
       <div class="xxs:col-span-12 xl:col-span-2 md:ml-3">
         <div class="grid grid-cols-12 xxs:my-8 my-0 gap-x-4">
 
@@ -85,17 +94,14 @@
             <p>Trend<em class="font-thin">ing</em></p>
           </div> 
 
-          <div class="col-span-12  mt-4 bg-blue flex xxs:flex-row xl:flex-col xxs:overflow-x-scroll md:overflow-hidden xxs:border-t md:border-none border-black">
+          <div class="col-span-12  mt-4 flex flex-row xl:flex-col xxs:overflow-x-scroll md:overflow-hidden">
 
-            <div class="xxs:w-auto md:w-9/12 xl:w-full mx-1 :xxs:border-b md:border-t border-black mt-3" v-for="i in 5 " :key="i">
+            <div class="mx-2 md:-mx-0 border-t border-black mt-3 w-3/4 md:w-full" v-for="i in 5 " :key="i">
               <time class="text-xs tracking-tight text-gray-700 my-2 mb-2 uppercase small-heading">Novemeber 23, 2020</time>
               <p class="text-base text-right leading-4 font-medium uppercase tracking-tighter text-gray-900 my-7">Isla Beauty Is Reimagining What Transparency Looks Like In Skincare</p>
             </div> 
-
-     
            
           </div>
-
 
          
         </div>
@@ -141,6 +147,17 @@ export default {
     width: 8rem;
 }
 
+.tSD{
+  height: 180px;
+  padding-bottom: 1rem;
+  padding-right: .5rem;
+}
+
+.atX{
+  line-height: 1.5rem !important;
+}
+
+
 @media (min-width: 768px){
   .hCont{
     font-size: 224px;
@@ -148,7 +165,6 @@ export default {
     line-height: .8;
   }
   .hCont > p{
-    position: absolute;
     font-family: PostGrotesk;
     display: inline-block;
     position: relative;  
